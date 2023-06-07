e26008196ababf23fec5697d384de51f1b4bb5c3
Erik Fischer <58076232+Battlemech@users.noreply.github.com>
2023-06-07 13:02:45 +0200

    initial commit

---
+++ Runtime/Test.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace Data_Management_for_Unity.Runtime
+{
+    public class Test : MonoBehaviour
+    {
+        void Start()
+        {
+            Debug.Log("Test behaviour started successfully!");
+        }
+    }
+}
