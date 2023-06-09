ee30f306fe80b19177bb5c28301d8e1473c0eccd
Erik Fischer <58076232+Battlemech@users.noreply.github.com>
2023-06-09 14:27:32 +0200

    added tests

--- Tests/EditMode/NetworkingTests.cs
+++ Tests/EditMode/NetworkingTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Threading;
+using Data_Management_for_Unity.Runtime;
 using Data_Management_for_Unity.Runtime.Networking.Messaging.Client;
 using Data_Management_for_Unity.Runtime.Networking.Messaging.Server;
 using Data_Management_for_Unity.Submodules.NetCoreServer;
@@ -18,8 +19,24 @@ namespace Data_Management_for_Unity.Tests.EditMode
         [Test]
         public static void TestSendString()
         {
-            string expected = "123456789";
-            string received = null;
+            TestSend("123431242345134521346231652462456245624562545642654225646254265424566254254625462654");
+        }
+
+        [Test]
+        public static void TestSendFloat()
+        {
+            TestSend(21321312321.1232123f);
+        }
+
+        [Test]
+        public static void TestSendClass()
+        {
+            TestSend(new TestObject("Detlef"));
+        }
+
+        private static void TestSend<T>(T expected)
+        {
+            ManualResetEvent receivedEvent = new ManualResetEvent(false);
             
             //start networking
             MessageServer server = new MessageServer("127.0.0.1", GetFreePort());
@@ -28,19 +45,45 @@ namespace Data_Management_for_Unity.Tests.EditMode
             MessageClient client = new MessageClient("127.0.0.1", server.Port);
             client.ConnectAsync();
             
+            /*
+             * Test Client send
+             */
+            
             //give client time to connect
             Assert.IsTrue(client.WaitForConnect());
 
             //Add callback, waiting for clients message
-            server.AddCallback<string>((s => received = s));
+            server.AddCallback<T>((s =>
+            {
+                Assert.AreEqual(expected, s);
+                
+                //allow waiting thread to continue
+                receivedEvent.Set();
+            }));
             
             //client sends message
             client.Send(expected);
             
-            //Wait for message to arrive
-            Thread.Sleep(1000);
+            Assert.IsTrue(receivedEvent.WaitOne(Options.DefaultTimeout));
             
-            Assert.AreEqual(expected, received);
+            /*
+             * Test Server send
+             */
+            
+            //reset event gate
+            receivedEvent.Reset();
+
+            client.AddCallback<T>((obj =>
+            {
+                Assert.AreEqual(expected, obj);
+
+                //allow waiting thread to continue
+                receivedEvent.Set();
+            }));
+
+            server.Multicast(expected);
+            
+            Assert.IsTrue(receivedEvent.WaitOne(Options.DefaultTimeout));
         }
     }
 }
