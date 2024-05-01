5e107944095b76c38420ba592fcb44a1eca8027e
Erik Fischer <58076232+Battlemech@users.noreply.github.com>
2024-05-01 15:11:28 +0200

    dictionary sync

--- Runtime/Databases/SynchronisedOperations/SynchronisedAdd.cs
+++ Runtime/Databases/SynchronisedOperations/SynchronisedAdd.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Data_Management_for_Unity.Runtime.Serializer;
+using MessagePack;
 
 namespace Data_Management_for_Unity.Runtime.Databases.SynchronisedOperations
 {
@@ -8,13 +9,23 @@ namespace Data_Management_for_Unity.Runtime.Databases.SynchronisedOperations
         where TCollection : ICollection<TValue>, new()
     {
         //serialize added value
+        [Key(4)]
         private readonly byte[] _addedValue;
+        [Key(5)]
         private readonly string _addedTypeString;
         
         public SynchronisedAdd(string databaseId, string valueId, byte[] addedValue, Type addedType, bool isSafe, Action<TCollection> onConfirmed) : base(databaseId, valueId, isSafe, onConfirmed)
         {
             _addedValue = addedValue;
             _addedTypeString = addedType.AssemblyQualifiedName;
+        }
+        
+        [SerializationConstructor]
+        protected SynchronisedAdd(string databaseId, string valueId, int modCount, bool isSafe, byte[] addedValue, string addedTypeString) : base(databaseId, valueId, isSafe, null)
+        {
+            ModCount = modCount;
+            _addedValue = addedValue;
+            _addedTypeString = addedTypeString;
         }
 
         protected override TCollection PerformAction(TCollection collection)
