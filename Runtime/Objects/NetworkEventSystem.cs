9da4e2321fe3793b8bec11f58db0a30570bc754a
Erik Fischer <58076232+Battlemech@users.noreply.github.com>
2024-06-13 15:55:53 +0200

    add network manager and network event system as prefabs

---
+++ Runtime/Objects/NetworkEventSystem.cs
@@ -0,0 +1,57 @@
+using System.Net.Sockets;
+using Data_Management_for_Unity.Runtime.Networking.Unity;
+using UnityEngine;
+using UnityEngine.Events;
+
+/// <summary>
+/// The client and server game objects aren't destroyed on scene transitions.
+/// Thus, they exists in different scenes, but their events can only be accessed via code.
+/// This class allows accessing it via Unity's event system.
+/// </summary>
+public class NetworkEventSystem : MonoBehaviour
+{
+    public UnityClient client;
+    public UnityServer server;
+    
+    //forward client events
+    public UnityEvent OnClientConnectingEvent;
+    public UnityEvent OnClientConnectedEvent;
+    public UnityEvent OnClientDisconnectingEvent ;
+    public UnityEvent OnClientDisconnectedEvent;
+    
+    //forward server events
+    public UnityEvent OnServerStartingEvent;
+    public UnityEvent OnServerStartedEvent;
+    public UnityEvent OnServerStoppingEvent;
+    public UnityEvent OnServerStoppedEvent;
+    public UnityEvent<SocketError> OnServerErrorEvent;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        //find client in scene
+        client ??= FindObjectOfType<UnityClient>();
+        
+        //find server in scene
+        server ??= FindObjectOfType<UnityServer>();
+        
+        //link events
+        LinkEvents();
+    }
+
+    private void LinkEvents()
+    {
+        //client events
+        client.OnConnectedEvent.AddListener(() => OnClientConnectedEvent.Invoke());
+        client.OnConnectingEvent.AddListener(() => OnClientConnectingEvent.Invoke());
+        client.OnDisconnectedEvent.AddListener(() => OnClientDisconnectedEvent.Invoke());
+        client.OnDisconnectingEvent.AddListener(() => OnClientDisconnectingEvent.Invoke());
+        
+        //server events
+        server.OnStartingEvent.AddListener(() => OnServerStartingEvent.Invoke());
+        server.OnStartedEvent.AddListener(() => OnServerStartedEvent.Invoke());
+        server.OnStoppedEvent.AddListener(() => OnServerStoppedEvent.Invoke());
+        server.OnStoppingEvent.AddListener(() => OnServerStoppingEvent.Invoke());
+        server.OnErrorEvent.AddListener((error) => OnServerErrorEvent.Invoke(error));
+    }
+}
