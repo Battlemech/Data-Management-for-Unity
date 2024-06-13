f9e50d0f6a64aaf3fe8c0bc9012e6274f456bcfc
Erik Fischer <58076232+Battlemech@users.noreply.github.com>
2024-06-13 18:06:25 +0200

    remove obsolete imports

--- Runtime/Databases/ValueStorages/ValueStorage.cs
+++ Runtime/Databases/ValueStorages/ValueStorage.cs
@@ -4,8 +4,6 @@ using System.Threading.Tasks;
 using Data_Management_for_Unity.Runtime.Databases.Structs;
 using Data_Management_for_Unity.Runtime.Serializer;
 using Data_Management_for_Unity.Runtime.Threading;
-using UnityEditor.VersionControl;
-using UnityEngine;
 using Task = System.Threading.Tasks.Task;
 
 namespace Data_Management_for_Unity.Runtime.Databases.ValueStorages
