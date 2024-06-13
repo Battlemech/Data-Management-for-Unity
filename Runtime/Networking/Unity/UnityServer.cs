267be1ec8d08e797d50fc347aab0f8bd90c25e12
Erik Fischer <58076232+Battlemech@users.noreply.github.com>
2024-06-13 15:31:20 +0200

    add description

--- Runtime/Networking/Unity/UnityServer.cs
+++ Runtime/Networking/Unity/UnityServer.cs
@@ -2,6 +2,10 @@
 
 namespace Data_Management_for_Unity.Runtime.Networking.Unity
 {
+    /// <summary>
+    /// Default implementation of the unity server wrapper.
+    /// Default session class is SynchronisedSession, unless changed by the user.
+    /// </summary>
     public class UnityServer : AbstractUnityServer<SynchronisedSession>
     {
         
